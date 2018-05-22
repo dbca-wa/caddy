4d9eb5131d602f64c584a63fd69bc02704621484
Ashley Felton <ashley.felton@dbca.wa.gov.au>
2018-05-22 13:52:31 +0800

    Update caddy.wsgi and .s2i/environment to work in container environment.

--- caddy/wsgi.py
+++ caddy/wsgi.py
@@ -2,10 +2,4 @@
 WSGI config for caddy project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 """
-import confy
-import os
-from django.core.wsgi import get_wsgi_application
-
-confy.read_environment_file('.env')
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'caddy.settings')
-application = get_wsgi_application()
+from geocoder import application
