1ecf982505522410cb4682c0d1d00674ede61a62
Ashley Felton <ashley@ropable.com>
2026-05-07 08:44:31 +0800

    Simplify Gunicorn config

--- gunicorn.py
+++ gunicorn.py
@@ -1,12 +1,7 @@
 # Gunicorn configuration settings.
-import multiprocessing
 
 bind = ":8080"
-# Don't start too many workers:
-workers = min(multiprocessing.cpu_count(), 4)
-# Give workers an expiry:
-max_requests = 2048
-max_requests_jitter = 256
+workers = 2
 preload_app = True
 # Disable access logging.
 accesslog = None
